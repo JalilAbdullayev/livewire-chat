c30e6e8fec191dfa78e8f79dc404085b8d8e0df2
Jalil <abdullacelil00@gmail.com>
2024-10-24 12:45:45 +0400

    feat(chat): implement message display
    - Added `loadMessages` method to fetch messages for a conversation.
    - Implemented message rendering with sender information and timestamp.

--- app/Livewire/Chat/ChatBox.php
+++ app/Livewire/Chat/ChatBox.php
@@ -8,16 +8,27 @@ use Livewire\Component;
 class ChatBox extends Component {
     public $selected;
     public $body;
+    public $loaded;
+
+    public function loadMessages() {
+        $this->loaded = Message::where('conversation_id', $this->selected->id)->get();
+    }
+
+    public function mount() {
+        $this->loadMessages();
+    }
 
     public function sendMessage() {
         $this->validate(['body' => 'required|string']);
-        Message::create([
+        $create = Message::create([
             'conversation_id' => $this->selected->id,
             'sender_id' => auth()->user()->id,
             'receiver_id' => $this->selected->getReceiver()->id,
             'body' => $this->body
         ]);
         $this->reset('body');
+        $this->dispatch('scroll-bottom');
+        $this->loaded->push($create);
     }
 
     public function render() {
--- resources/views/livewire/chat/chat-box.blade.php
+++ resources/views/livewire/chat/chat-box.blade.php
@@ -1,4 +1,7 @@
-<div class="w-full overflow-hidden">
+<div x-data="{height: 0, element: document.getElementById('conversation')}" x-init="
+height = element.scrollHeight;
+$nextTick(() => element.scrollTop = height)" @scroll-bottom.window="$nextTick(() => element.scrollTop = height)"
+     class="w-full overflow-hidden">
     <div class="border-b flex flex-col overflow-y-scroll grow h-full">
         <header class="sticky w-full inset-x-0 flex py-[5px] top-0 z-10 bg-white border-b">
             <div class="flex w-full items-center px-2 lg:px-4 gap-2 md:gap-5">
@@ -16,47 +19,63 @@
                 </h6>
             </div>
         </header>
-        <main
-            class="flex flex-col gap-3 p-2.5 overflow-y-auto flex-grow overscroll-contain overflow-x-hidden w-full my-auto">
-            <div @class(['max-w-[85%] md:max-w-[78%] gap-2 flex w-auto relative mt-2'])>
-                <div @class(['shrink-0'])>
-                    <x-avatar/>
-                </div>
-                <div @class(['flex flex-wrap text-[15px] rounded-xl p-2.5 flex flex-col text-black bg-[#F6F6F8FB]',
-                'rounded-bl-none border border-gray-200/40' => false, 'rounded-br-none bg-blue-500/80 text-white' => true])>
-                    <p class="whitespace-normal truncate text-sm md:text-base tracking-wide lg:tracking-normal">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eum id incidunt quas! Accusantium
-                        animi, aspernatur beatae commodi cupiditate error harum praesentium reiciendis suscipit vitae?
-                        Assumenda enim laboriosam nemo tempore!
-                    </p>
-                    <div class="ml-auto flex gap-2">
-                        <p @class(['text-xs', 'text-gray-500' => false, 'text-white' => true])>
-                            5:25am
-                        </p>
-                        <div>
-                        <span @class(['text-gray-500'])>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                 viewBox="0 0 16 16">
-                                  <path
-                                      d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0"/>
-                                  <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708"/>
-                            </svg>
-                        </span>
-                            {{--<span @class(['text-gray-500'])>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                     viewBox="0 0 16 16">
-                                      <path
-                                          d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
-                                </svg>
-                            </span>--}}
+        <main id="conversation"
+              class="flex flex-col gap-3 p-2.5 overflow-y-auto flex-grow overscroll-contain overflow-x-hidden w-full my-auto">
+            @if($loaded)
+                @php($previous = null)
+                @foreach($loaded as $key => $message)
+                    @if($key > 0)
+                        @php($previous = $loaded->get($key-1))
+                    @endif
+                    <div @class(['max-w-[85%] md:max-w-[78%] gap-2 flex w-auto relative mt-2', 'ml-auto' => $message->sender_id === auth()->user()->id])>
+                        <div @class(['shrink-0', 'invisible' => $previous?->sender_id === $message->sender_id,
+                            'hidden' => $message->sender_id === auth()->user()->id])>
+                            <x-avatar/>
+                        </div>
+                        <div @class(['flex flex-wrap text-[15px] rounded-xl p-2.5 flex flex-col text-black bg-[#F6F6F8FB]',
+                            'rounded-bl-none border border-gray-200/40' => !($message->sender_id === auth()->user()->id),
+                            'rounded-br-none bg-blue-500/80 text-white' => $message->sender_id === auth()->user()->id])>
+                            <p class="whitespace-normal truncate text-sm md:text-base tracking-wide lg:tracking-normal">
+                                {{ $message->body }}
+                            </p>
+                            <div class="ml-auto flex gap-2">
+                                <p @class(['text-xs', 'text-gray-500' => !($message->sender_id === auth()->user()->id),
+                                    'text-white' => $message->sender_id === auth()->user()->id])>
+                                    {{ $message->created_at->format('g:i') }}
+                                </p>
+                                @if($message->sender_id === auth()->user()->id)
+                                    <div>
+                                        @if($message->isRead())
+                                            <span @class(['text-gray-200'])>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                     fill="currentColor" viewBox="0 0 16 16">
+                                                      <path
+                                                          d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0"/>
+                                                      <path
+                                                          d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708"/>
+                                                </svg>
+                                            </span>
+                                        @else
+                                            <span @class(['text-gray-200'])>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                     fill="currentColor" viewBox="0 0 16 16">
+                                                      <path
+                                                          d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0"/>
+                                                </svg>
+                                            </span>
+                                        @endif
+                                    </div>
+                                @endif
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                @endforeach
+            @endif
         </main>
         <footer class="shrink-0 z-10 bg-white inset-x-0">
             <div class="p-2 border-t">
-                <form method="POST" autocapitalize="off" x-data="{body: $wire.entangle('body')}" @submit.prevent="$wire.sendMessage">
+                <form method="POST" autocapitalize="off" x-data="{body: $wire.entangle('body')}"
+                      @submit.prevent="$wire.sendMessage">
                     @csrf
                     <input type="hidden" autocomplete="off" class="hidden"/>
                     <div class="grid grid-cols-12">
